c6aa15094c76a9684d07baddd40189a005f38bad
ankhang24112411-sudo <ankhang24112411@gmail.com>
2026-08-10 17:28:41 +0200

    state pattern intergrate

---
+++ src/main/java/com/khang/backendecommerce/newstruc/domain/suborder/config/SubOrderStateContext.java
@@ -0,0 +1,31 @@
+package com.khang.backendecommerce.newstruc.domain.suborder.config;
+
+import com.khang.backendecommerce.newstruc.domain.suborder.*;
+import com.khang.backendecommerce.newstruc.entity.SubOrderEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SubOrderStateContext {
+    private final PendingState pendingState;
+    private final ConfirmedState confirmedState;
+    private final PickingState pickingState;
+    private final ShippingState shippingState;
+    private final DeliveredState deliveredState;
+    private final FailedState failedState;
+    private final ReturningState returningState;
+    private final ReattemptState reattemptState;
+    public SubOrderState getState(SubOrderEntity subOrder) {
+        return switch (subOrder.getOrderStatus()) {
+            case PENDING -> pendingState;
+            case CONFIRMED -> confirmedState;
+            case PICKING -> pickingState;
+            case SHIPPING -> shippingState;
+            case DELIVERED -> deliveredState;
+            case FAILED -> failedState;
+            case RETURNING -> returningState;
+            case REATTEMPT -> reattemptState;
+        };
+    }
+}
---
+++ src/main/java/com/khang/backendecommerce/newstruc/domain/suborder/config/SubOrderStateService.java
@@ -0,0 +1,58 @@
+package com.khang.backendecommerce.newstruc.domain.suborder.config;
+
+import com.khang.backendecommerce.newstruc.domain.suborder.SubOrderState;
+import com.khang.backendecommerce.newstruc.entity.SubOrderEntity;
+import com.khang.backendecommerce.newstruc.repo.SubOrderRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SubOrderStateService {
+
+    private final SubOrderRepository subOrderRepository;
+    private final SubOrderStateContext stateContext;
+
+    @Transactional
+    public void confirm(SubOrderEntity subOrder) {
+        SubOrderState state = stateContext.getState(subOrder);
+        state.confirm(subOrder);
+    }
+
+    @Transactional
+    public void startPicking(SubOrderEntity subOrder) {
+        SubOrderState state = stateContext.getState(subOrder);
+        state.startPicking(subOrder);
+    }
+
+    @Transactional
+    public void startShipping(SubOrderEntity subOrder) {
+        SubOrderState state = stateContext.getState(subOrder);
+        state.startShipping(subOrder);
+    }
+
+    @Transactional
+    public void delivered(SubOrderEntity subOrder) {
+        SubOrderState state = stateContext.getState(subOrder);
+        state.delivered(subOrder);
+    }
+
+    @Transactional
+    public void deliveryFailed(SubOrderEntity subOrder) {
+        SubOrderState state = stateContext.getState(subOrder);
+        state.failed(subOrder);
+    }
+    @Transactional
+    public void returning(SubOrderEntity subOrder) {
+        SubOrderState state = stateContext.getState(subOrder);
+        state.returning(subOrder);
+    }
+
+    @Transactional
+    public void reattempt(SubOrderEntity subOrder) {
+        SubOrderState state = stateContext.getState(subOrder);
+        state.reattempt(subOrder);
+    }
+
+}
